58759674fd0b86e5833ad7fe77c2d6a5aca6a2da
rafafuentesz <rafafuentes110@gmail.com>
2025-09-15 11:36:13 -0300

    updated register component

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,33 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent { }
+export class RegisterPageComponent {
+
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(FormUtils.namePattern)]],
+    email: ['', [Validators.required, Validators.pattern(FormUtils.emailPattern)],
+           [FormUtils.checkingServerResponse]
+  ],
+    username: ['', [Validators.required, Validators.minLength(3), Validators.pattern(FormUtils.notOnlySpacesPattern)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]],
+  }, {
+    validators: this.formUtils.matchFields('password', 'confirmPassword')
+  } )
+
+  onSubmit() {
+    console.log(this.myForm.value);
+    this.myForm.markAllAsTouched();
+  }
+
+}
